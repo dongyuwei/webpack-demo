2bd069ee1c5632f8b8a1e1d6622f9f27c31cb246
Dong Yuwei <newdongyuwei@gmail.com>
2019-04-21 14:10:10 +0800

    config output filename see https://webpack.js.org/configuration/output/

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
+const path = require("path");
+
 module.exports = {
   entry: {
     foo: "./src/entry-foo.js",
     foo2015: "./src/entry-foo-es2015-module.js"
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[contenthash].bundle.js"
   },
   optimization: {
     minimize: false
