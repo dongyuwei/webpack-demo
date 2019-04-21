96f8fc9f554e3ae1c2fd95cef7b0905114caf90f
Dong Yuwei <newdongyuwei@gmail.com>
2019-04-21 14:02:08 +0800

    minimize config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   entry: {
-    foo: './src/entry-foo.js',
-    foo2015: './src/entry-foo-es2015-module.js',
+    foo: "./src/entry-foo.js",
+    foo2015: "./src/entry-foo-es2015-module.js"
+  },
+  optimization: {
+    minimize: false
   }
 };
-
