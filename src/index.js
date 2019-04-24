f9a3262e7c586103266b0b7326fb2caef296dcb0
Dong Yuwei <newdongyuwei@gmail.com>
2019-04-24 17:20:56 +0800

    import icon in js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import _ from "lodash";
 import $ from "jquery";
 import style from "./index.less";
+import icon from "./icon.svg";
 console.log("====================================");
-console.log(style);
+console.log(style, icon);
 console.log("====================================");
 
 function render() {
   const element = $(`<div class=${style.foo}></div>`);
   element.text(_.join(["Hello", "webpack"], " ")).appendTo(document.body);
+
+  const img = new Image();
+  img.src = icon;
+  $("body").append(img);
 }
 
 render();
